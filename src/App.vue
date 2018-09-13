478bbebd42ab69bda728f0fdf5049835bf2a3cfc
arsalantezro <arsalan@tezrosolutions.com>
2018-09-13 20:24:15 +0500

    first

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,25 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <test></test>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+// import HelloWorld from './components/HelloWorld.vue'
+import Test from "./components/test";
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    Test
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/test.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="test">
+        <input type="text" v-model="title">
+        <br>
+        <!-- <h1>{{title}}</h1> -->
+       <!-- {{title}} -->
+        <span v-html="title"></span>
+        <p>{{user.first_name}}</p>
+        <br>
+        <p>{{user.last_name}}</p>
+        <p v-if="showName" v-text="user.first_name"></p>
+        <p v-else v-text="user.last_name"></p>
+        <ul>
+            <!-- eslint-disable-next-line  -->
+            <li v-for="item in items">{{item.title}}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "test",
+  data() {
+    return {
+      title: "<h1>Hello World</h1>",
+      user: {
+        first_name: "john",
+        last_name: "doe"
+      },
+      showName: false,
+      items: [{ title: "item 1 " }, { title: "item 2 " }, { title: "item 3 " }]
+    };
+  }
+};
+</script>
+<style scoped>
+</style>
