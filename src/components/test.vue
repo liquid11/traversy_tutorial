9c2fd12a2ed9a0a77e83840ce09355a638970f61
arsalantezro <arsalan@tezrosolutions.com>
2018-09-13 21:11:45 +0500

    user actions

--- src/components/test.vue
+++ src/components/test.vue
@@ -10,10 +10,12 @@
         <p>{{user.last_name}}</p>
         <p v-if="showName" v-text="user.first_name"></p>
         <p v-else v-text="user.last_name"></p>
-        <ul>
-            <!-- eslint-disable-next-line  -->
+        <!-- <ul>
+        
             <li v-for="item in items">{{item.title}}</li>
-        </ul>
+        </ul> -->
+     <button v-on:click="greet">action</button>
+     <input type="text" v-on:keyup="presskey" v-on="keyup.enter="enterkey"/>
     </div>
 </template>
 
@@ -22,7 +24,8 @@ export default {
   name: "test",
   data() {
     return {
-      title: "<h1>Hello World</h1>",
+      //   title: "<h1>Hello World</h1>",
+      title: "Hello World",
       user: {
         first_name: "john",
         last_name: "doe"
@@ -30,6 +33,19 @@ export default {
       showName: false,
       items: [{ title: "item 1 " }, { title: "item 2 " }, { title: "item 3 " }]
     };
+  },
+  methods: {
+    greet: function() {
+      alert("HELLO");
+    },
+    presskey: function(e) {
+      //    console.log("key pressed");
+      console.log(e.target.value);
+    },
+    enterkey: function(e) {
+      //    console.log("key pressed");
+      console.log("the value entered is " + e.target.value);
+    }
   }
 };
 </script>
