759f20389a1beeb7f479221989cb6b0a7f76578e
HP <HP@DESKTOP-CUOUS0T>
2018-09-22 03:05:26 +0500

    vue router

--- src/main.js
+++ src/main.js
@@ -1,10 +1,37 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
-import  vueResource from 'vue-resource'
+import vueResource from 'vue-resource'
+import Users from './components/Users'
+import test from './components/test'
 
 Vue.config.productionTip = false
 
 Vue.use(vueResource)
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  mode: 'history',
+  base: '__dirname',
+  routes: [
+    { path: '/', component: Users },
+    { path: '/test', component: test }
+
+  ]
+})
+// new Vue({
+//   router,
+//   render: h => h(App)
+// }).$mount('#app')
 new Vue({
-  render: h => h(App)
-}).$mount('#app')
+  router,
+  template: `
+  <div>
+  <ul>
+  <li><router-link to="/">Users</router-link></li>
+  <li><router-link to="/test">Test</router-link></li>
+</ul>
+<router-view></router-view>
+</div>`,
+    render: h => h(App)
+}).$mount('#app');
